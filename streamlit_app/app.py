5d8868fcb9e0a126f7652ef3fc4a03279a1d2105
PyBluePanda <alohabluepanda@gmail.com>
2024-10-22 12:45:40 +0100

    Adding Logo

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -4,7 +4,6 @@ st.set_page_config(layout="wide")
 
 pg = st.navigation(
     [st.Page("homepage.py", title="Hackacthon Details", icon=":material/add_circle:"),
-     st.Page("abs.py", title="Agent Based Simulation", icon=":material/people:"),
      st.Page("mesa_abs.py", title="Mesa Agent Based Simulation", icon=":material/people:"),
      ]
      )
